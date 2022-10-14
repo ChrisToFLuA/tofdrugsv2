ddf15aaf15f62de8768c13f40e2a97430bba2f89
ChrisToF29380 <109798768+ChrisToFLuA@users.noreply.github.com>
2022-10-14 09:00:33 +0200

    Update tdgsinit.lua

--- server/tdgsinit.lua
+++ server/tdgsinit.lua
@@ -169,3 +169,23 @@ AddEventHandler('playerDropped', function (reason)
         end
     end
 end)
+
+---- Github Updater ----
+
+function GetCurrentVersion()
+	return GetResourceMetadata( GetCurrentResourceName(), "version" )
+end
+
+local CurrentVersion = GetCurrentVersion()
+local resourceName = "^4["..GetCurrentResourceName().."]^0"
+
+PerformHttpRequest('https://raw.githubusercontent.com/ChrisToFLuA/tofdrugsv2/main/VERSION', function(Error, NewestVersion, Header)
+	print("###############################")
+    if CurrentVersion == NewestVersion then
+	    print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
+    elseif CurrentVersion ~= NewestVersion then
+        print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
+        print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://raw.githubusercontent.com/ChrisToFLuA/tofdrugsv2/releases/tag/v'.. NewestVersion .. '^0')
+    end
+	print("###############################")
+end)
